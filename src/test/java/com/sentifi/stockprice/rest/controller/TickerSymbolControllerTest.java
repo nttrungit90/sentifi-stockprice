40d76cc9741c21b33bcc370cb4d79ded49a391ea
nttrungit90 <nttrungit90@gmail.com>
2017-05-22 13:10:50 +0700

    Add test for An invalid ticker symbol returns a easy to understand error and HTTP Code of 404

--- src/test/java/com/sentifi/stockprice/rest/controller/TickerSymbolControllerTest.java
+++ src/test/java/com/sentifi/stockprice/rest/controller/TickerSymbolControllerTest.java
@@ -1,10 +1,12 @@
 package com.sentifi.stockprice.rest.controller;
 
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,6 +32,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.sentifi.StockPriceService;
 import com.sentifi.stockprice.business.service.TickerSymbolService;
+import com.sentifi.stockprice.exception.ErrorCode;
+import com.sentifi.stockprice.exception.StockPriceException;
 import com.sentifi.stockprice.vo.ClosePrice;
 import com.sentifi.stockprice.vo.TickerSymbolClosePrice;
 
@@ -58,35 +62,34 @@ private MockMvc mockMvc;
         
 	}
 
+	// ############ Start test cases for requirement 1 ######################
 	@Test
-	public void verifyGetTickerSymbolClosePriceAllParametersSuccess() throws Exception {
-		//prepare test data
-		String tickerSymbol = "GE";
-		String startDate = "2017-02-13";
-		String endDate = "2017-02-15";
-		
-		TickerSymbolClosePrice tickerSymbolClosePrice = new TickerSymbolClosePrice();
-		tickerSymbolClosePrice.setTicker(tickerSymbol);
-		List<ClosePrice> closeDates  = Arrays.asList(
-				new ClosePrice("2017-02-13", 30.04), 
-				new ClosePrice("2017-02-14", 30.28),
-				new ClosePrice("2017-02-15", 30.35));
-		tickerSymbolClosePrice.setCloseDates(closeDates);
+	public void verifyGetTickerSymbolClosePriceAllParamsSuccess() throws Exception {
 		/*
 		Expected returned Json
 		{
 		    "Prices": [
 			    {
 				    "Ticker": "GE",
-				    "DateClose": ["1999-02-13", "20.1"],
-				    "DateClose": ["1999-02-14", "20.2"],
-				    "DateClose": ["1999-02-15", "20.3"]
+				    "DateClose": ["2017-02-13", "30.04"],
+				    "DateClose": ["2017-02-14", "30.28"],
+				    "DateClose": ["2017-02-15", "30.35"]
 			    }
 		    ]
 		}
-
 		 */
 		
+		//prepare test data
+		String tickerSymbol = "GE";
+		String startDate = "2017-02-15";
+		String endDate = "2017-02-15";
+		
+		TickerSymbolClosePrice tickerSymbolClosePrice = new TickerSymbolClosePrice();
+		tickerSymbolClosePrice.setTicker(tickerSymbol);
+		List<ClosePrice> closeDates  = Arrays.asList(new ClosePrice("2017-02-15", 30.35));
+		tickerSymbolClosePrice.setCloseDates(closeDates);
+		
+		// mock test data
         when(tickerSymbolServiceMock.getTickerSymbolClosePrice(tickerSymbol, startDate, endDate)).thenReturn(tickerSymbolClosePrice);
  
         UriComponentsBuilder builder = UriComponentsBuilder.fromPath("/api/v2/");
@@ -95,15 +98,62 @@ private MockMvc mockMvc;
 		builder.queryParam("endDate", endDate);
 		URI uri = builder.build().encode().toUri();
 
+		// execute test
         mockMvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON))
         		.andExpect(content().contentType(APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.Prices.[0].Ticker", is(tickerSymbol)));
+                .andExpect(jsonPath("$.Prices.[0].Ticker", is(tickerSymbol)))
+                .andExpect(jsonPath("$.Prices.[0].DateClose[0]", is("2017-02-15")));
  
         verify(tickerSymbolServiceMock, times(1)).getTickerSymbolClosePrice(tickerSymbol, startDate, endDate);
         verifyNoMoreInteractions(tickerSymbolServiceMock);
         
 	}
+	
+	
+	@Test
+	/*
+	 * An invalid ticker symbol returns a easy to understand error and HTTP Code of 404
+	 */
+	public void verifyGetTickerSymbolClosePriceInvalidTickerSymbol() throws Exception {
+		/*
+		Expected returned Json
+		{
+		    "errors": {},
+		    "stockPriceError": {
+		        "code": "QECx02",
+		        "message": "You have submitted an incorrect Quandl code. Please check your Quandl codes and try again."
+	    	}
+		}
+		*/
+		
+		//prepare test data
+		String tickerSymbol = "invalidTickerSymbol";
+		String startDate = "2017-02-15";
+		String endDate = "2017-02-15";
+		StockPriceException stockPriceException = new StockPriceException(ErrorCode.QECx02);
+	
+		
+		// mock test data
+		when(tickerSymbolServiceMock.getTickerSymbolClosePrice(eq(tickerSymbol), anyString(), anyString())).thenThrow(stockPriceException);
+        
+		UriComponentsBuilder builder = UriComponentsBuilder.fromPath("/api/v2/");
+		builder.path(tickerSymbol + "/closePrice"); 
+		builder.queryParam("startDate", startDate);
+		builder.queryParam("endDate", endDate);
+		URI uri = builder.build().encode().toUri();
+
+		// execute test
+        mockMvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON))
+        		.andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(status().is(404))
+                .andExpect(jsonPath("$.stockPriceError.code", is("QECx02")));
+ 
+        verify(tickerSymbolServiceMock, times(1)).getTickerSymbolClosePrice(tickerSymbol, startDate, endDate);
+        verifyNoMoreInteractions(tickerSymbolServiceMock);
+	}
+	
+	// ############ End test cases for requirement 1 ######################
 	
 	@Test
 	public void verifyGetTickerSymbol200DayMovingAvgClosePrice() throws Exception {
